86d909b381e4f8c72c26fa85a0c5bebe099e3927
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-31 12:02:39 -0300

    feat: adding a route for the about me page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home/indes"
+import HeaderBar from "./components/header"
+import AboutMe from "./Pages/AboutMe"
+import Home from "./Pages/Home"
 
 const AppRouter = () => {
     return(
         <BrowserRouter>
+        <HeaderBar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/aboutMe" element={<AboutMe />} />
             </Routes>
         </BrowserRouter>
     )
