d3f452fffeee2521e15b433f7d2105cf02762890
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-31 10:41:11 -0300

    feat: creating the Home page component folder

---
+++ src/Pages/Home/indes.jsx
@@ -0,0 +1,11 @@
+import { HomeWrapper } from "./style"
+
+const Home = () => {
+    return(
+        <HomeWrapper>
+            <p>Ola Mundo</p>
+        </HomeWrapper>
+    )
+}
+
+export default Home
