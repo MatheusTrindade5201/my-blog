6cf825b1f7550d56bfda395c795c94c9d5e28aff
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-31 12:00:38 -0300

    feat: creating a component for the About Me page

---
+++ src/Pages/AboutMe/index.jsx
@@ -0,0 +1,11 @@
+import { AboutMeWrapper } from "./style"
+
+const AboutMe = () => {
+    return(
+        <AboutMeWrapper>
+            <p>About Me</p>
+        </AboutMeWrapper>
+    )
+}
+
+export default AboutMe
