788c7059c36cf69e6b060008cfdba582a5a19206
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-01-01 18:56:12 -0300

    creating a global style

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HeaderBar from "./components/header"
 import AboutMe from "./Pages/AboutMe"
 import Home from "./Pages/Home"
+import GlobalStyle from "./style/globalStyle"
 
 const AppRouter = () => {
     return(
@@ -11,6 +12,7 @@ const AppRouter = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/aboutMe" element={<AboutMe />} />
             </Routes>
+            <GlobalStyle />
         </BrowserRouter>
     )
 }
