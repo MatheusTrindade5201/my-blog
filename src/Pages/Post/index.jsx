d4862316fc79a5fd88581d4f762232f80d41ca86
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-01-05 20:01:55 -0300

    feat: creating the dinamic page for the posts

--- src/Pages/Post/index.jsx
+++ src/Pages/Post/index.jsx
@@ -1,9 +1,31 @@
+import { useParams } from "react-router-dom"
 import { PostPageWrapper } from "./style"
+import ReactMarkdown from "react-markdown";
+
+import posts from '@/json/posts.json';
+import PostBody from "@/components/postBody";
 
 const Post = () => {
+
+    const path = useParams();
+    const post = posts.find((post) => {
+        return post.id === Number(path.id)
+    })
+
+    if(!post){
+        return <h1 style={{marginTop: '2.5rem'}}>Post not found</h1>
+    }
+
     return(
         <PostPageWrapper>
-            
+            <PostBody 
+            bannerPath={`/posts/${post.id}/capa.png`} 
+            title={post.titulo}
+            >
+                <ReactMarkdown className="post-markdown-container">
+                    {post.texto}
+                </ReactMarkdown>
+            </PostBody>
         </PostPageWrapper>
     )
 }
